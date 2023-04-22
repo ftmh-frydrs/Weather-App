15d1c1dd45335dab05fea760ddc992f4e2daa01a
Fatemeh <fatemehfaryadras7@gmail.com>
2023-04-23 00:18:00 +0330

    third commit

--- content/js/script.js
+++ content/js/script.js
@@ -1,17 +1,42 @@
 const apiKey = "ef28bcaad68fa1480f257d224b552f4d";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=bangalore";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
-async function AppWeather(){
-     const response = await fetch (apiUrl + `&appid=${apiKey}`);
+const searchEl = document.querySelector(".search input");
+const btnEl = document.querySelector(".search button");
+const iconEl = document.querySelector(".icon");
+
+async function AppWeather(city){
+     const response = await fetch (apiUrl+ city +`&appid=${apiKey}`);
      var data = await response.json();
      console.log(data);
      document.querySelector(".city").innerHTML = data.name;
-     document.querySelector(".temp").innerHTML = data.main.temp;
-     document.querySelector(".humidity").innerHTML = data.main.humidity;
-     document.querySelector(".wind").innerHTML = data.wind.speed     
+     document.querySelector(".temp").innerHTML = Math.round(data.main.temp) +"°C";
+     document.querySelector(".humidity").innerHTML = data.main.humidity +"%";
+     document.querySelector(".wind").innerHTML = data.wind.speed +" Km/h" ;   
+     
+     if(data.weather[0].main == "Clear"){
+     iconEl.src="./content/img/clear.png"
+  }else if(data.weather[0].main == "Clouds"){
+     iconEl.src="./content/img/clouds.png"
+  }else if(data.weather[0].main == "Rain"){
+     iconEl.src="./content/img/rain.png"
+  }else if(data.weather[0].main == "Drizzle"){
+     iconEl.src="./content/img/drizzle.png"
+  }else if(data.weather[0].main == "Mist"){
+     iconEl.src="./content/img/mist.png"
+  }
+
+  document.querySelector(".hidden").style.display="block"
 }
 
-AppWeather();
+btnEl.addEventListener("click", ()=>{
+     AppWeather(searchEl.value);
+})
+
+
+
+
+
 
 ////http://api.openweathermap.org/data/2.5/forecast/daily?APPID=12345&q=.
 
